bebf07a34a315b55b2a1aa66b5d87d36accdf750
cleiton <djcleitonduarte@hotmail.com>
2021-04-29 21:29:12 -0400

    Criado interfaceRepository da Entidade: Historico

---
+++ src/main/java/com/estoqueBebidas/repository/HistoricoRepository.java
@@ -0,0 +1,9 @@
+package com.estoqueBebidas.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.estoqueBebidas.entities.Historico;
+
+public interface HistoricoRepository extends JpaRepository<Historico, Integer> {
+
+}
