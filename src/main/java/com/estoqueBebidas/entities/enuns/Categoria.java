d746d1680a73d7f7e339fa6a3fec128eae8afa47
cleiton <djcleitonduarte@hotmail.com>
2021-04-29 20:33:26 -0400

    Criado ENUM Categoria

---
+++ src/main/java/com/estoqueBebidas/entities/enuns/Categoria.java
@@ -0,0 +1,39 @@
+package com.estoqueBebidas.entities.enuns;
+
+public enum Categoria {
+
+	ALCOOLICA(1,500),
+	NAOALCOOLICA(2,400);
+	
+	private int cod;
+	private double limit;
+	
+	private Categoria(int cod, double limit) {
+		this.cod = cod;
+		this.limit = limit;
+	}
+
+	public int getCod() {
+		return cod;
+	}
+
+	public void setCod(int cod) {
+		this.cod = cod;
+	}
+
+	public double getLimit() {
+		return limit;
+	}
+
+	public void setLimit(double limit) {
+		this.limit = limit;
+	}
+	public static Categoria convertCategoria(int cod) {
+		for (Categoria x : Categoria.values()) {
+			if(x.getCod() == cod) {
+				return x;
+			}
+		}
+		throw new IllegalArgumentException("Categoria não encontrada. Código informado: "+cod);
+	}
+}
