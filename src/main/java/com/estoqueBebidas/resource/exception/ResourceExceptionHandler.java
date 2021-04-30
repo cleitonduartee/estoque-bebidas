2901fd954ce551dfb7b1a5e48131cc8189963d18
cleiton <djcleitonduarte@hotmail.com>
2021-04-30 02:20:05 -0400

    Criado Classes: StandardError e ResourceExceptionHandler na camada de resource

---
+++ src/main/java/com/estoqueBebidas/resource/exception/ResourceExceptionHandler.java
@@ -0,0 +1,22 @@
+package com.estoqueBebidas.resource.exception;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import com.estoqueBebidas.service.exception.ResourceNotFoundException;
+
+@ControllerAdvice
+public class ResourceExceptionHandler {
+
+	@ExceptionHandler(ResourceNotFoundException.class)
+	public ResponseEntity<StandardError> resourceNotFound(ResourceNotFoundException e, HttpServletRequest request){
+		HttpStatus status = HttpStatus.NOT_FOUND;
+		String error = "Não foi possível encontrar o recurso solicitado. ";
+		StandardError err = new StandardError(status.value(), error, e.getMessage(), request.getRequestURI());
+		return ResponseEntity.status(status).body(err);
+	}
+}
