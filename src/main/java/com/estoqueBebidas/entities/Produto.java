4548dc6339d4eaef217afed37e89b943aaf35cf1
cleiton <djcleitonduarte@hotmail.com>
2021-04-29 20:38:01 -0400

    Criado entidade Produto

---
+++ src/main/java/com/estoqueBebidas/entities/Produto.java
@@ -0,0 +1,84 @@
+package com.estoqueBebidas.entities;
+
+import java.io.Serializable;
+
+import com.estoqueBebidas.entities.enuns.Categoria;
+
+public class Produto implements Serializable {
+	private static final long serialVersionUID = 1L;
+	
+	private Integer id;
+	private String nome;
+	private Integer categoria;
+	
+	public Produto() {		
+	}
+
+	public Produto(Integer id, String nome, Categoria categoria) {
+		super();
+		this.id = id;
+		this.nome = nome;
+		this.categoria = categoria != null ? categoria.getCod():null;
+	}	
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public Categoria getCategoria() {
+		return Categoria.convertCategoria(categoria);
+	}
+
+	public void setCategoria(Categoria categoria) {
+		this.categoria = categoria.getCod();
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((categoria == null) ? 0 : categoria.hashCode());
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		result = prime * result + ((nome == null) ? 0 : nome.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Produto other = (Produto) obj;
+		if (categoria != other.categoria)
+			return false;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		if (nome == null) {
+			if (other.nome != null)
+				return false;
+		} else if (!nome.equals(other.nome))
+			return false;
+		return true;
+	}
+	
+
+	
+}
