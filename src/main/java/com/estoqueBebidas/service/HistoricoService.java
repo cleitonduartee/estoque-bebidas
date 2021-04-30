147258782b716668a07ea262e4e49cf1d92d3ad1
cleiton <djcleitonduarte@hotmail.com>
2021-04-29 22:11:01 -0400

    Criado camada de Service das Entidades : Historico, Produto e Secao

---
+++ src/main/java/com/estoqueBebidas/service/HistoricoService.java
@@ -0,0 +1,9 @@
+package com.estoqueBebidas.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class HistoricoService {
+
+	
+}
---
+++ src/main/java/com/estoqueBebidas/service/SecaoService.java
@@ -0,0 +1,8 @@
+package com.estoqueBebidas.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class SecaoService {
+
+}
