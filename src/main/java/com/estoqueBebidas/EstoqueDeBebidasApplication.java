0dabc58256c38776093e0a6d972d5705ddc331c9
cleiton <djcleitonduarte@hotmail.com>
2021-04-29 20:22:44 -0400

    Projeto iniciado

---
+++ src/main/java/com/estoqueBebidas/EstoqueDeBebidasApplication.java
@@ -0,0 +1,13 @@
+package com.estoqueBebidas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EstoqueDeBebidasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EstoqueDeBebidasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/estoqueBebidas/EstoqueDeBebidasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.estoqueBebidas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EstoqueDeBebidasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
